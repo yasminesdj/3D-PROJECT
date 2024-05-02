40627b8686ceb558de414d8f924ec741e0538125
yasminesdj <yasminesaidj319@gmail.com>
2024-05-02 15:23:38 +0100

    update

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+/*import React from "react";
 import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
 import { Suspense } from "react";
 import { Track } from "./Track";
 import { Ground } from "./Ground";
 import { Car } from "./Car";
-import Button from "./Button";
 
 export function Scene() {
   return (
@@ -22,7 +21,59 @@ export function Scene() {
       <Track />
       <Ground />
       <Car />
-      <Button />
+    </Suspense>
+  );
+} */
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Suspense, useEffect, useState } from "react";
+import { Car } from "./Car";
+import { Ground } from "./Ground";
+import { Track } from "./Track";
+ import R3FButton from "./R3FButton";
+
+
+export function Scene() {
+  const [thirdPerson, setThirdPerson] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+
+  useEffect(() => {
+    function keydownHandler(e) {
+      if (e.key === "k") {
+        // random is necessary to trigger a state change
+        if(thirdPerson) setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+        setThirdPerson(!thirdPerson); 
+      }
+    }
+
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler);
+  }, [thirdPerson]);
+
+  return (
+    <Suspense fallback={null}>
+      <Environment
+        files={process.env.PUBLIC_URL + "/textures/envmap (1).hdr"}
+        background={"both"}
+      />
+
+      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      {!thirdPerson && (
+        <OrbitControls    enableZoom={false}
+        enablePan={false}
+        enableDamping={true}
+        dampingFactor={0.25}
+        rotateSpeed={0.5}target={[-2.64, -0.71, 0.03]} />
+      )}
+
+      <Ground />
+      <Track />
+      <R3FButton />
+      <Car thirdPerson={thirdPerson} />
+    
     </Suspense>
   );
 }
